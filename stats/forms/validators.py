1e43617ec32b1c62e139dfc490f2840c0edd9cd8
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2019-12-11 21:28:44 +0100

    Add form validator for is_safename text utility

---
+++ stats/forms/validators.py
@@ -0,0 +1,14 @@
+from wtforms import ValidationError
+
+from stats.lib.text import is_safename
+
+
+class SafeName:
+    def __init__(self, message=None):
+        if message is None:
+            message = 'This is not a safe name'
+        self.message = message
+
+    def __call__(self, _, field):
+        if not is_safename(field.data):
+            raise ValidationError(self.message)
