f8dd702f91ef782914e5ec54c651ee765c184f6f
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-04-07 13:33:49 +0200

    Use the @rollup/plugin instead of the rollup-plugin because versions are newer

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import cssimport from 'postcss-import';
 import cssprefixer from 'autoprefixer';
 import cssurl from 'postcss-url';
 import path from 'path';
 import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
