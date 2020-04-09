7cb940f8e2f936fa6ca7faf81b84df80da749723
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-04-09 13:33:20 +0200

    Add explicit tests for the application object

---
+++ tests/test_app.py
@@ -0,0 +1,50 @@
+from observatory.lib.cli import BP_CLI
+from observatory.rest.mapper import BP_REST_MAPPER
+from observatory.rest.prompt import BP_REST_PROMPT
+from observatory.rest.sensor import BP_REST_SENSOR
+from observatory.shared import errorhandler, moment_config, tagline
+from observatory.start.environment import ERROR_CODES
+from observatory.start.extensions import CSRF_PROTECT, DB, MIGRATE
+from observatory.views.main import BLUEPRINT_MAIN
+from observatory.views.mgnt import BLUEPRINT_MGNT
+from observatory.views.side import BLUEPRINT_SIDE
+from observatory.views.user import BLUEPRINT_USER
+
+
+class TestApp:
+
+    @staticmethod
+    def test_extensions(app):
+        assert app.extensions['csrf'] == CSRF_PROTECT
+
+        mig_conf = app.extensions['migrate']
+        assert mig_conf.migrate == MIGRATE
+
+        sql_stat = app.extensions['sqlalchemy']
+        assert sql_stat.db == DB
+
+    @staticmethod
+    def test_errorhandler(app):
+        handlers = app.error_handler_spec[None]
+        for code in ERROR_CODES:
+            for handler in handlers[code].values():
+                assert handler is errorhandler
+
+    @staticmethod
+    def test_blueprints(app):
+        blueprints = [
+            BP_CLI,
+            BLUEPRINT_MAIN, BLUEPRINT_MGNT,
+            BLUEPRINT_SIDE, BLUEPRINT_USER,
+            BP_REST_MAPPER, BP_REST_PROMPT, BP_REST_SENSOR,
+        ]
+        for blueprint in app.blueprints.values():
+            assert blueprint in blueprints
+            blueprints.remove(blueprint)
+
+        assert not blueprints
+
+    @staticmethod
+    def test_template_functions(app):
+        assert app.jinja_env.globals['moment_config'] is moment_config
+        assert app.jinja_env.globals['tagline'] is tagline
