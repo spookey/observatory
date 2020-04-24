0ed36b5ce5e4f20de5a22aa8bc7061986f2f03eb
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-04-24 08:54:00 +0200

    Rewrite dropdown handling - allow them to close by clicking anywhere on page

--- assets/dropdowns.ts
+++ assets/dropdowns.ts
@@ -2,17 +2,19 @@
 export function dropToggle(): void {
   document.addEventListener("DOMContentLoaded", (): void => {
     const ACT: string = "is-active";
+    const dropdowns: HTMLCollection = document.getElementsByClassName("dropdown");
 
-    function register(dropdown: HTMLElement): void {
-      for (const trigger of dropdown.getElementsByClassName("dropdown-trigger") as any) {
-        trigger.addEventListener("click", (): void => {
-          dropdown.classList.toggle(ACT);
-        });
+    for (const dropdown of dropdowns as any) {
+      dropdown.addEventListener("click", (event: Event): void => {
+        event.stopPropagation();
+        dropdown.classList.toggle(ACT);
+      });
+    }
+
+    document.body.addEventListener("click", (): void => {
+      for (const dropdown of dropdowns as any) {
+        dropdown.classList.remove(ACT);
       }
-    }
-
-    for (const dropdown of document.getElementsByClassName("dropdown") as any) {
-      register(dropdown);
-    }
+    });
   });
 }
