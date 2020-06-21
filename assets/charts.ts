87842fb416bfb9f599de430593d84e48c9c1f75a
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-06-21 15:10:35 +0200

    Adjust template and charts script to render the fixed values of the chart similar to those from the template

--- assets/charts.ts
+++ assets/charts.ts
@@ -13,6 +13,15 @@ import conf from "./settings";
 import { lighten } from "./colors";
 import { soften } from "./colors";
 
+interface DisplayInfo {
+  plain: object;
+  logic: object;
+}
+
+interface DispChartDataSets extends ChartDataSets {
+  display?: DisplayInfo;
+};
+
 
 const baseChartConfig = (): ChartConfiguration => ({
   type: "line",
@@ -70,34 +79,78 @@ const baseChartConfig = (): ChartConfiguration => ({
 class Graph {
   private slug: string;
   private bar: HTMLProgressElement;
+  private template: HTMLTemplateElement;
+  private bucket: HTMLElement;
   private chart: Chart;
+
   private config: AxiosRequestConfig = {
     baseURL: conf.apiPlotBaseUrl,
     method: "get",
     responseType: "json",
     timeout: 10 * 1000,
-  }
+  };
 
   constructor(
     slug: string,
     bar: HTMLProgressElement,
-    ctx: CanvasRenderingContext2D,
+    template: HTMLTemplateElement,
+    bucket: HTMLElement,
+    context: CanvasRenderingContext2D,
   ) {
     this.slug = slug;
     this.bar = bar;
-    this.chart = new Chart(ctx, baseChartConfig());
+    this.template = template;
+    this.bucket = bucket;
+    this.chart = new Chart(context, baseChartConfig());
   }
 
-  private attach(payload: ChartDataSets[]): void {
+  private showBar() { this.bar.classList.remove("is-invisible"); }
+  private hideBar() { this.bar.classList.add("is-invisible"); }
+  private showBucket() { this.bucket.classList.remove("is-hidden"); }
+  private hideBucket() { this.bucket.classList.add("is-hidden"); }
+  private clearBucket() {
+    for (const child of this.bucket.children as any) {
+      this.bucket.removeChild(child);
+    }
+  }
+
+  private paint(display: DisplayInfo): void {
+    const clone: HTMLTemplateElement =
+      this.template.cloneNode(true) as HTMLTemplateElement;
+
+    function set(sect: string, name: string, value: string): void {
+      const sel: string = `span.${sect}-${name}`
+      for(const elem of clone.content.querySelectorAll(sel) as any) {
+        if (elem instanceof HTMLElement) {
+          elem.replaceWith(document.createTextNode(value));
+        }
+      }
+    }
+
+
+    for (const [name, value] of Object.entries(display.plain) as any) {
+      set('plain', name, (value as string));
+    }
+
+
+    this.bucket.appendChild(clone.content);
+    clone.classList.remove("is-hidden");
+  }
+
+  private attach(payload: DispChartDataSets[]): void {
     if (!this.chart.data || !this.chart.data.datasets) { return; }
     this.chart.data.datasets = [];
 
     for (const idx in payload) {
       if (payload.hasOwnProperty(idx)) {
-        const obj: ChartDataSets = payload[idx];
+        const obj: DispChartDataSets = payload[idx];
         if (obj.borderColor) {
           obj.borderColor = soften(obj.borderColor as string);
           obj.backgroundColor = lighten(obj.borderColor as string);
+        }
+        if (obj.display) {
+          this.paint(obj.display);
+          delete obj.display;
         }
         this.chart.data.datasets[idx] = obj;
       }
@@ -106,15 +159,14 @@ class Graph {
     this.chart.update({duration: 0});
   }
 
-  private showBar(): void { this.bar.classList.remove("is-invisible"); }
-  private hideBar(): void { this.bar.classList.add("is-invisible"); }
-
   private refresh(): void {
     this.showBar();
+    this.hideBucket();
+    this.clearBucket();
 
     axios.get(this.slug, this.config)
       .then((res: AxiosResponse): void => {
-        this.attach(res.data as ChartDataSets[]);
+        this.attach(res.data as DispChartDataSets[]);
       })
       .catch((err: AxiosError): void => {
         // tslint:disable-next-line
@@ -122,6 +174,7 @@ class Graph {
       })
       .finally((): void => {
         this.hideBar();
+        this.showBucket();
       });
   }
 
@@ -138,8 +191,12 @@ export function drawCharts(): void {
       return container.dataset.slug;
     }
 
-    function getBar(container: HTMLElement): (HTMLProgressElement | null) {
-      for (const element of container.getElementsByTagName("progress") as any) {
+    function getBar(
+      container: HTMLElement): (HTMLProgressElement | null
+    ) {
+      for (
+        const element of container.getElementsByTagName("progress") as any
+      ) {
         if (element instanceof HTMLProgressElement) {
           return element;
         }
@@ -147,8 +204,38 @@ export function drawCharts(): void {
       return null;
     }
 
-    function getCtx(container: HTMLElement): (CanvasRenderingContext2D | null) {
-      for (const element of container.getElementsByTagName("canvas") as any) {
+    function getTemplate(
+      container: HTMLElement): (HTMLTemplateElement | null
+    ) {
+      for (
+        const element of container.getElementsByTagName("template") as any
+      ) {
+        if (element instanceof HTMLTemplateElement) {
+          return element;
+        }
+      }
+      return null;
+    }
+
+    function getBucket(
+      container: HTMLElement): (HTMLElement | null
+    ) {
+      for (
+        const element of container.getElementsByClassName("bucket") as any
+      ) {
+        if (element instanceof HTMLElement) {
+          return element;
+        }
+      }
+      return null;
+    }
+
+    function getContext(
+      container: HTMLElement): (CanvasRenderingContext2D | null
+    ) {
+      for (
+        const element of container.getElementsByTagName("canvas") as any
+      ) {
         if (element instanceof HTMLCanvasElement) {
           return (element as HTMLCanvasElement).getContext("2d");
         }
@@ -161,10 +248,14 @@ export function drawCharts(): void {
       if (!slug) { return; }
       const bar: (HTMLProgressElement | null) = getBar(container);
       if (!bar) { return; }
-      const ctx: (CanvasRenderingContext2D | null) = getCtx(container);
-      if (!ctx) { return; }
+      const template: (HTMLTemplateElement | null) = getTemplate(container);
+      if (!template) { return; }
+      const bucket: (HTMLElement | null) = getBucket(container);
+      if (!bucket) { return; }
+      const context: (CanvasRenderingContext2D | null) = getContext(container);
+      if (!context) { return; }
 
-      new Graph(slug, bar, ctx).loop();
+      new Graph(slug, bar, template, bucket, context).loop();
     }
 
     for (const container of document.querySelectorAll(".plot") as any) {
